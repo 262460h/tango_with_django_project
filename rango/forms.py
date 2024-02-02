93678cd7e09d44f5a5d8d4c34ba6baee4f11c52e
Madison Horvath <2620460H@student.gla.ac.uk>
2024-02-02 12:03:09 +0000

    Ch7 Passes Tests

--- rango/forms.py
+++ rango/forms.py
@@ -5,7 +5,7 @@ class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    ilkes = forms.IntegerField(widget=forms.HiddenInput(), intial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
@@ -13,7 +13,7 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128
+    title = forms.CharField(max_length=128,
                             help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
@@ -21,5 +21,14 @@ class PageForm(forms.ModelForm):
 
     class Meta:
         model = Page
-
         exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+
+        return cleaned_data
